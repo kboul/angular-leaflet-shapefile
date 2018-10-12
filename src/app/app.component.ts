a6289c16831e3a3742b622a7773bcdd6f44546ee
kboul <kos.voulgaridis@gmail.com>
2018-10-12 11:24:59 +0300

    Initialize a basic leaflet map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'leaflet';
+declare let L;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-leaflet-shapefile';
+export class AppComponent implements OnInit{
+	ngOnInit() {
+		const map = L.map('map').setView([60, 100], 3);
+
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		  attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+	}
 }
