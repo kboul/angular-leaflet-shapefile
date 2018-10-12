a8825c3595c1cdb212db566e7cddeed770a0901d
kboul <kos.voulgaridis@gmail.com>
2018-10-12 12:12:03 +0300

    Convert shapefile to GeoJson & add it to the map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import 'leaflet';
+import * as shp from 'shpjs';
 declare let L;
 
 @Component({
@@ -7,12 +8,21 @@ declare let L;
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 	ngOnInit() {
-		const map = L.map('map').setView([60, 100], 3);
-
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		  attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
+		const m = L.map('map').setView([34.74161249883172, 18.6328125], 2);
+      	const geo = L.geoJson({features: []}, { onEachFeature: function popUp(f, l) {
+			const out = [];
+			if (f.properties) {
+				for (const key of Object.keys(f.properties)) {
+					out.push(key + ' : ' + f.properties[key]);
+				}
+				l.bindPopup(out.join('<br />'));
+			}
+		}}).addTo(m);
+		const base = 'assets/TM_WORLD_BORDERS_SIMPL-0.3.zip';
+			shp(base).then(function(data) {
+				geo.addData(data);
+		});
 	}
 }
